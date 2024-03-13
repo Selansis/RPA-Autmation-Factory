290c405d23dac22b95c69f857ef2215844c52422
Selansis <wojtekmichalik24@gmail.com>
2024-03-13 19:02:40 +0100

    downloading the file and saving to xlsx

--- RPA Automation Factory.py
+++ RPA Automation Factory.py
@@ -1,4 +1,7 @@
 import requests
-import pandas
-df = pandas.read_excel(r"https://rpachallenge.com/assets/downloadFiles/challenge.xlsx")
-print(df)
+dls = "https://rpachallenge.com/assets/downloadFiles/challenge.xlsx"
+resp = requests.get(dls)
+
+output = open('test.xlsx', 'wb')
+output.write(resp.content)
+output.close()
